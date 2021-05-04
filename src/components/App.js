51e8e105fac7aa674dd6303e84f4436934f83c98
cjl72 <cjl72@njit.edu>
2021-05-04 16:26:09 -0400

    Task: Created basic Navigation.js component

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Dashboard from './Dashboard';
+import Transactions from './Transactions';
+import Navigation from './Navigation';
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
+                <Navigation />
                 <div>
                     <Route path="/" exact component={ Dashboard } />
+                    <Route path="/transactions" exact component={ Transactions } />
                 </div>
             </BrowserRouter>
         </div>
