e086f1ffd3a3645364aa4b9c8aff4d14f4b5f60d
cjl72 <cjl72@njit.edu>
2021-05-04 21:13:20 -0400

    Task: Have cards being rendered with test data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import './styles/dashboard.css';
+
 
 class Dashboard extends React.Component {
 
-    renderList() {
-        let accountList = [this.props.accounts];
+    state = {
+        accountsList: [{id: 1, name: 'Michael', balance: 20, transactions: []},
+            {id: 2, name: 'Jim', balance: 5000, transactions: []},
+            {id: 3, name: 'Pam', balance: 6000, transactions: []},
+            {id: 4, name: 'Dwight', balance: 20000, transactions: []},
+            {id: 5, name: 'Stanley', balance: 600000, transactions: []},
+            {id: 6, name: 'Kevin', balance: 3000, transactions: []}]
+    }
 
-        return accountList.map((char, index) => {
+    renderList() {
+
+        return this.state.accountsList.map((char, index) => {
            return(
              <div className='card' key={index}>
-                 <div className='card-body'>
-                     { char.name }
+                 <div className='card-body justify-content-center'>
+                     <h2 className='card-header'>{ char.name }</h2>
                      <Link to ={ `/account/${char.id}` }>
                          <button type='button'
                                  className='btn btn-primary'>
@@ -29,9 +39,7 @@ class Dashboard extends React.Component {
         return (
             <div className="dashboard container text-center">
                 <h1>Dashboard</h1>
-                <ul className='list-group'>
-                    { accountList }
-                </ul>
+                { accountList }
             </div>
         )
     }
