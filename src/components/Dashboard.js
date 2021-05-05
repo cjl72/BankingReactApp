2310c6a773380814d601c32dfd03101921a405f1
cjl72 <cjl72@njit.edu>
2021-05-04 20:14:56 -0400

    Task: Created accounts that will be displayed on the Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Dashboard = props => {
-    return (
-        <div className="dashboard container text-center">
-            <h1>Dashboard</h1>
-            <ul className='list-group'>
-                List of Accounts
-            </ul>
-        </div>
-    )
+class Dashboard extends React.Component {
+
+    renderList() {
+        let accountList = this.props[this.props.accounts];
+
+        return accountList.map((char, index) => {
+           return(
+             <div className='card' key={index}>
+                 <div className='card-body'>
+                     { char.name }
+                     <Link to ={ `/account/${char.id}` }>
+                         <button type='button'
+                                 className='btn btn-primary'>
+                             Account Details
+                         </button>
+                     </Link>
+                 </div>
+             </div>
+           );
+        });
+    }
+
+    render() {
+        const accountList = this.renderList();
+        return (
+            <div className="dashboard container text-center">
+                <h1>Dashboard</h1>
+                <ul className='list-group'>
+                    { accountList }
+                </ul>
+            </div>
+        )
+    }
 }
 
-export default Dashboard;
+const mapStateToProps = state => {
+    return {
+      accounts: state.accounts
+    };
+};
+
+export default connect(mapStateToProps, null)(Dashboard);
