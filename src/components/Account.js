6f4532aeac8ec19ff47e8ac3f68b6ec16c1f442e
cjl72 <cjl72@njit.edu>
2021-05-04 21:38:12 -0400

    Task: Updated Cards to balance, delete button, and updated headers

--- src/components/Account.js
+++ src/components/Account.js
@@ -2,10 +2,41 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class Account extends React.Component {
+    state = {
+        id: null,
+        name: '',
+        balance: null,
+        transactions: [],
+        accountsList: [{id: 1, name: 'Michael', balance: 20, transactions: []},
+            {id: 2, name: 'Jim', balance: 5000, transactions: []},
+            {id: 3, name: 'Pam', balance: 6000, transactions: []},
+            {id: 4, name: 'Dwight', balance: 20000, transactions: []},
+            {id: 5, name: 'Stanley', balance: 600000, transactions: []},
+            {id: 6, name: 'Kevin', balance: 3000, transactions: []},
+            {id: 7, name: 'Phyllis', balance: 20000, transactions: []},
+            {id: 8, name: 'Andy', balance: 600000, transactions: []},
+            {id: 9, name: 'Darryl', balance: 3000, transactions: []}
+        ]
+    }
+
+    componentDidMount() {
+        this.getDataFromProps();
+    }
+
+    getDataFromProps() {
+        const id = this.props.match.params.id;
+        const account = this.state.accountsList.find(char => char.id.toString() === id.toString());
+        this.setState({ id: account.id, name: account.name,
+            balance: account.balance, transactions: account.transactions
+        });
+    }
+
+
     render() {
         return (
           <div className='container text-center'>
-              Account: {this.props.match.params.id}
+              <h1>Account: {this.props.match.params.id}</h1>
+              <h2>{this.state.name}</h2>
           </div>
         );
     };
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,14 +8,14 @@ class Dashboard extends React.Component {
 
     state = {
         accountsList: [{id: 1, name: 'Michael', balance: 20, transactions: []},
-            {id: 2, name: 'Jim', balance: 5000, transactions: []},
-            {id: 3, name: 'Pam', balance: 6000, transactions: []},
-            {id: 4, name: 'Dwight', balance: 20000, transactions: []},
-            {id: 5, name: 'Stanley', balance: 600000, transactions: []},
-            {id: 6, name: 'Kevin', balance: 3000, transactions: []},
-            {id: 7, name: 'Phyllis', balance: 20000, transactions: []},
-            {id: 8, name: 'Andy', balance: 600000, transactions: []},
-            {id: 9, name: 'Darryl', balance: 3000, transactions: []}
+            {id: 2, name: 'Jim', balance: '5,000', transactions: []},
+            {id: 3, name: 'Pam', balance: '6,000', transactions: []},
+            {id: 4, name: 'Dwight', balance: '20,000', transactions: []},
+            {id: 5, name: 'Stanley', balance: '600,000', transactions: []},
+            {id: 6, name: 'Kevin', balance: '3,000', transactions: []},
+            {id: 7, name: 'Phyllis', balance: '20,000', transactions: []},
+            {id: 8, name: 'Andy', balance: '600,000', transactions: []},
+            {id: 9, name: 'Darryl', balance: '3,000', transactions: []}
         ]
     }
 
@@ -25,13 +25,18 @@ class Dashboard extends React.Component {
            return(
              <div className='card' key={index}>
                  <div className='card-body justify-content-center'>
-                     <h2 className='card-header'>{ char.name }</h2>
+                     <h2 className='card-header'>{ char.name }'s <br /> Account</h2>
+                     <h4>Balance: ${ char.balance } </h4> <br />
                      <Link to ={ `/account/${char.id}` }>
                          <button type='button'
                                  className='btn btn-primary'>
                              Account Details
                          </button>
                      </Link>
+                     <button type='button'
+                             className='btn btn-danger'>
+                         Delete
+                     </button>
                  </div>
              </div>
            );
